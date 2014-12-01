37c2a89d882bc6395c057d0e51871ac637cf943b
linaizhong <linaizhong@gmail.com>
2014-12-01 11:12:47 +1100

    Fixing bugs since using ala-web-theme:0.8.1

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -53,8 +53,7 @@ grails.project.dependency.resolution = {
         runtime ":richui:0.8"
         runtime ":tiny-mce:3.4.4"
         runtime ":cached-resources:1.0"
-        runtime ":cors:1.1.6"
+        //runtime ":cors:1.1.6"
         runtime ":ala-web-theme:0.8.1"
-        //runtime ":ala-web-theme:0.2.4"
     }
 }

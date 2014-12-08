f13a5d7442476d82531367efd901b7499dd1ac31
linaizhong <linaizhong@gmail.com>
2014-12-08 12:41:51 +1100

    modify production setting in config.groovy

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -260,21 +260,6 @@ environments {
 
 logging.dir = (System.getProperty('catalina.base') ? System.getProperty('catalina.base') + '/logs'  : '/var/log/tomcat6')
 
-//// log4j configuration
-//log4j = {
-//    error  'org.codehaus.groovy.grails.web.servlet',        // controllers
-//           'org.codehaus.groovy.grails.web.pages',          // GSP
-//           'org.codehaus.groovy.grails.web.sitemesh',       // layouts
-//           'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
-//           'org.codehaus.groovy.grails.web.mapping',        // URL mapping
-//           'org.codehaus.groovy.grails.commons',            // core / classloading
-//           'org.codehaus.groovy.grails.plugins',            // plugins
-//           'org.codehaus.groovy.grails.orm.hibernate',      // hibernate integration
-//           'org.springframework',
-//           'org.hibernate',
-//           'net.sf.ehcache.hibernate'
-//}
-
 log4j = {
     appenders {
         environments {

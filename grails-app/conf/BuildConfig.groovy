a465a89104c6f58ce68d8398a6417c89b6d42ef6
nickdos <nickdos@gmail.com>
2016-01-07 15:46:19 +1100

    Version 1.3.6
    collectory plugin version 1.5.4

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -38,7 +38,7 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        runtime ":collectory:1.5.3"
+        runtime ":collectory:1.5.4"
         runtime ":cached-resources:1.0"
         build   ":release:3.0.1"
         runtime ":ala-bootstrap2:2.4"

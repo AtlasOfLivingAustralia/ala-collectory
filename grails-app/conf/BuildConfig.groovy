f8a6830da0ee8aa77e68e4239c236261f9c2100f
temi <temivarg@gmail.com>
2016-07-27 10:40:16 +1000

    AtlasOfLivingAustralia/sandbox#28 collectory plugin version - 1.5.12-SNAPSHOT

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -38,7 +38,7 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        runtime ":collectory:1.5.8"
+        runtime ":collectory:1.5.12-SNAPSHOT"
         runtime ":cached-resources:1.0"
         build   ":release:3.0.1"
         runtime ":ala-bootstrap2:2.4.1"

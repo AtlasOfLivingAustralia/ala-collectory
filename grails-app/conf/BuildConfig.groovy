be4d60883ca6611d8815b5741c9c11ac454ed567
Dave Martin <djtfmartin@gmail.com>
2021-07-29 16:26:49 +0100

    collectory:1.9.11 minor CSS fixes i18n properties

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -38,7 +38,7 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        runtime ":collectory:1.9.11-SNAPSHOT"
+        runtime ":collectory:1.9.11"
         runtime ":ala-bootstrap3:2.2.0"
         runtime ":resources:1.2.14"
         runtime ":cached-resources:1.0"
